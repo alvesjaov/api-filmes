0bb86c248815853d681230c9cd7ff29185e1ac61
alvesjaov <victoralves169@outlook.com>
2023-10-23 13:51:16 -0300

    refactor: atualizar a função de renderização do React

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import App from './App.jsx'
@@ -9,12 +9,12 @@ import Search from './pages/Search.jsx'
 
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
+        <Route element={<App />}>
+          <Route path="/" element={<Home />} />
           <Route path="movie/:id" element={<Movie />} />
           <Route path="search" element={<Search />} />
         </Route>
