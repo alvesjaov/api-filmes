4be3c80fb95ad94f030b989e4642f05e1e02dd3f
alvesjaov <victoralves169@outlook.com>
2023-11-03 12:19:52 -0300

    Add: funcionalidade de pesquisa para o componente Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom'
 import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
 
 import './Navbar.css';
 
 function Navbar() {
+    const [search, setSearch] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!search) return;
+        navigate(`/search?q=${search}`);
+        setSearch('');
+    }
+
     return (
         <div>
             <nav id="navbar">
@@ -12,15 +23,19 @@ function Navbar() {
                         <BiCameraMovie />MoviesLib
                     </Link>
                 </h2>
-                <form>
-                    <input type="text" placeholder="Busque um filme" />
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Busque um filme"
+                        onChange={(e) => setSearch(e.target.value)}
+                        value={search}
+                    />
                     <button type="submit">
                         <BiSearchAlt2 />
                     </button>
                 </form>
             </nav>
         </div>
-    )
+    );
+
 }
 
 export default Navbar
