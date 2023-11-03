05ca19f888c8494d7c3607b11c1087fd97c3e76a
alvesjaov <victoralves169@outlook.com>
2023-11-03 17:43:31 -0300

    Add: seleção de gênero de filme e paginação

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,102 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import MoviesCard from '../components/MoviesCard';
 
 import './MoviesGrid.css';
 
-const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
-
+const moviesURL = import.meta.env.VITE_API;
+const genreURL = import.meta.env.VITE_GENRE;
 
 const Home = () => {
-  const [topMovies, setTopMovies] = useState([]);
-
-  const getTopRatedMovies = async (url) => {
-
-    const res = await fetch(url);
-    const data = await res.json();
-
-    setTopMovies(data.results);
+  const [movies, setMovies] = useState([]);
+  const [genre, setGenre] = useState('top_rated');
+  const [page, setPage] = useState(1);
+  const genreNames = {
+    'top_rated': 'Melhores Filmes',
+    '28': 'Ação',
+    '12': 'Aventura',
+    '16': 'Animação',
+    '35': 'Comédia',
+    '80': 'Crime',
+    '99': 'Documentário',
+    '18': 'Drama',
+    '10751': 'Família',
+    '14': 'Fantasia',
+    '36': 'História',
+    '27': 'Terror',
+    '10402': 'Música',
+    '9648': 'Mistério',
+    '10749': 'Romance',
+    '878': 'Ficção científica',
+    '10770': 'Cinema TV',
+    '53': 'Thriller',
+    '10752': 'Guerra',
+    '37': 'Faroeste'
   };
 
-  useEffect(() => {
-    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+  const getMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+  
+    setMovies(oldMovies => {
+      // Cria um novo conjunto de filmes, excluindo quaisquer duplicatas
+      const newMovies = [...oldMovies, ...data.results].reduce((acc, current) => {
+        const x = acc.find(item => item.id === current.id);
+        if (!x) {
+          return acc.concat([current]);
+        } else {
+          return acc;
+        }
+      }, []);
+      return newMovies;
+    });
+  };
+  
 
-    getTopRatedMovies(topRatedUrl);
-  }, []);
+  useEffect(() => {
+    const url = genre === 'top_rated' ? `${moviesURL}top_rated?${apiKey}&page=${page}` : `${genreURL}?${apiKey}&with_genres=${genre}&page=${page}`;
+
+    getMovies(url);
+  }, [genre, page]);
+
+  const handleGenreChange = (newGenre) => {
+    if (newGenre !== genre) {
+      setMovies([]);
+      setPage(1);
+      setGenre(newGenre);
+    }
+  };
+
+  const loadMoreMovies = () => {
+    setPage(oldPage => oldPage + 1);
+  };
+
 
   return (
     <div className="container">
-      <h2 className="title">Melhores Filmes</h2>
-      <div className="movies-container">
-        {topMovies.length === 0 && <p>Carregando...</p>}
-        {topMovies.length > 0 && topMovies.map((movie) => <MoviesCard key={movie.id} movie={movie} />)}
+      <div className="content">
+        <div className="title-container">
+          <select
+            name="genre"
+            value={genre}
+            onChange={(e) => handleGenreChange(e.target.value)}
+          >
+            {Object.entries(genreNames).map(([value, name]) => (
+              <option key={value} value={value}>
+                {name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <h2 className="title">{genreNames[genre]}</h2>
+        <div className="movies-container">
+          {movies.length === 0 && <p>Carregando...</p>}
+          {movies.length > 0 && movies.map((movie) => <MoviesCard key={movie.id} movie={movie} />)}
+        </div>
+        {movies.length > 0 && <button onClick={loadMoreMovies}>Carregar mais</button>}
       </div>
     </div>
   );
 }
 
-export default Home
+export default Home;
