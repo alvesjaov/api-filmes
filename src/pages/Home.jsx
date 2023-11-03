f1816b943830072a7616b4ef047009dd59e12e53
alvesjaov <victoralves169@outlook.com>
2023-11-02 22:30:59 -0300

    feat: adiciona estilo css home e define MovieGrid.css para reaproveitamento

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import MoviesCard from '../components/MoviesCard';
 
+import './MoviesGrid.css';
+
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
@@ -25,7 +27,7 @@ const Home = () => {
   return (
     <div className="container">
       <h2 className="title">Melhores Filmes</h2>
-      <div className="movie-container">
+      <div className="movies-container">
         {topMovies.length === 0 && <p>Carregando...</p>}
         {topMovies.length > 0 && topMovies.map((movie) => <MoviesCard key={movie.id} movie={movie} />)}
       </div>
