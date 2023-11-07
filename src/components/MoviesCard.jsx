33828056ae738eb3a8eb6c89d5ca6a726b7b4b49
alvesjaov <victoralves169@outlook.com>
2023-11-07 20:38:34 -0300

    Refactor: componente MoviesCard para usar a imagem padrão se poster_path for null

--- src/components/MoviesCard.jsx
+++ src/components/MoviesCard.jsx
@@ -1,19 +1,28 @@
 import { Link } from "react-router-dom";
 import { FaStar } from 'react-icons/fa';
 import PropTypes from 'prop-types';
+import defaultImage from '../assets/img/placeholder.svg'; // Importa a imagem padrão
 
 const imageUrl = import.meta.env.VITE_IMG;
-
+const notImg = defaultImage;
 
 const MoviesCard = ({ movie, showLink = true }) => {
+    // Se movie.poster_path for nulo, usa a imagem padrão
+    const posterPath = movie.poster_path ? imageUrl + movie.poster_path : notImg;
+    const isDefaultImage = posterPath === notImg;
+
     return (
         <div className="movie-card">
-            <img src={imageUrl + movie.poster_path} alt={movie.title} />
+            <img src={posterPath} alt={movie.title} className={isDefaultImage ? "no-events" : ""} />
             <h2>{movie.title}</h2>
-            <p>
-                <FaStar /> {movie.vote_average.toFixed(1)}
-            </p>
-            {showLink && <Link to={`/movie/${movie.id}`}>Ver detalhes</Link>}
+            {showLink && (
+                <>
+                    <p>
+                        <FaStar /> {movie.vote_average.toFixed(1)}
+                    </p>
+                    <Link to={`/movie/${movie.id}`}>Ver detalhes</Link>
+                </>
+            )}
         </div>
     );
 }
