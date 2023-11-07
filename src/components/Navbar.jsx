977f37dc88f06cee6ddcff13f3a7d6fc633ef663
alvesjaov <victoralves169@outlook.com>
2023-11-07 20:42:28 -0300

    Add: funcionalidade de pesquisa de filmes ao componente Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,52 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { BiSearchAlt2 } from 'react-icons/bi';
+import logo from '../assets/img/Stars.svg';
 
 import './Navbar.css';
 
+const apiKey = import.meta.env.VITE_API_KEY;
+const searchUrl = import.meta.env.VITE_SEARCH;
+const lang = import.meta.env.VITE_LANG;
+
+
 function Navbar() {
     const [search, setSearch] = useState('');
+    const [results, setResults] = useState([]);
     const navigate = useNavigate();
+    const inputRef = useRef();
+
+    const fetchMovies = async (query) => {
+        if (!query) return;
+        const response = await fetch(`${searchUrl}?${apiKey}&language=${lang}&query=${query}`);
+        const data = await response.json();
+
+        // Cria um objeto para armazenar os títulos dos filmes
+        const titles = {};
+
+        // Adiciona cada título ao objeto como uma chave
+        data.results.forEach(result => {
+            titles[result.title] = result;
+        });
+
+        // Converte o objeto de volta para um array
+        const uniqueResults = Object.values(titles);
+
+        setResults(uniqueResults);
+    }
+
+    useEffect(() => {
+        fetchMovies(search);
+    }, [search]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         if (!search) return;
         navigate(`/search?q=${search}`);
-        setSearch('');
+        setTimeout(() => {
+            setSearch('');
+            setResults([]);
+        }, 100);
     }
 
     return (
@@ -20,24 +54,29 @@ function Navbar() {
             <nav id="navbar">
                 <h2>
                     <Link to="/">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bibi-stars" viewBox="0 0 16 16">
-                            <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828l.645-1.937zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.734 1.734 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.734 1.734 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.734 1.734 0 0 0 3.407 2.31l.387-1.162zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L10.863.1z" />
-                        </svg>Space+
+                        <img src={logo} alt="Logo" className='bibi-stars' />Space+
                     </Link>
                 </h2>
+
                 <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder="Busque um filme"
+                    <input type="text" placeholder="Pesquise um filme" list="movie-titles"
                         onChange={(e) => setSearch(e.target.value)}
                         value={search}
+                        ref={inputRef}
+                        name="movieSearch"  /* Adicionado atributo name */
                     />
-                    <button type="submit">
+                    <datalist id="movie-titles">
+                        {results.map((result) => (
+                            <option key={result.id} value={result.title} />
+                        ))}
+                    </datalist>
+                    <button type="submit" title='buscar'>
                         <BiSearchAlt2 />
                     </button>
                 </form>
             </nav>
         </div>
     );
-
 }
 
 export default Navbar
