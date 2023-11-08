4a3824866815d99044583dbf90587347d343b52e
alvesjaov <victoralves169@outlook.com>
2023-11-07 22:49:28 -0300

    Fix: Caminhos de arquivos e formatação

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import {Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <div className="App">
-       <Navbar/>
-        <Outlet/>
-      </div>
-    </>
+    <div className="App">
+      <Navbar />
+      <Outlet />
+    </div>
   )
 }
 
