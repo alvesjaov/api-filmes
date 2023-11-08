aa9f2390781e20d3da627273dde35a66b91ee012
alvesjaov <victoralves169@outlook.com>
2023-11-08 10:03:33 -0300

    Remove: autocomplete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,55 +1,15 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { BiSearchAlt2 } from 'react-icons/bi';
 import Stars from '../../assets/img/Stars.svg';
 
 import './Navbar.css';
 
-const apiKey = import.meta.env.VITE_API_KEY;
-const searchUrl = import.meta.env.VITE_SEARCH;
-const lang = import.meta.env.VITE_LANG;
-
 function Navbar() {
     const [search, setSearch] = useState('');
-    const [results, setResults] = useState([]);
     const navigate = useNavigate();
     const inputRef = useRef();
     const formRef = useRef();
-    const [showResults, setShowResults] = useState(false);
-
-
-    const fetchMovies = async (query) => {
-        if (!query) return;
-        const response = await fetch(`${searchUrl}?${apiKey}&language=${lang}&query=${query}`);
-        const data = await response.json();
-        // Cria um objeto para armazenar os títulos dos filmes
-        const titles = {};
-
-        // Adiciona cada título ao objeto como uma chave
-        data.results.forEach(result => {
-            titles[result.title] = result;
-        });
-
-        // Converte o objeto de volta para um array
-        const uniqueResults = Object.values(titles);
-
-        setResults(uniqueResults);
-    }
-
-    useEffect(() => {
-        fetchMovies(search);
-    }, [search]);
-
-    const handleChange = (e) => {
-        const input = e.target.value;
-        setSearch(input);
-        setShowResults(true);
-    };
-
-    const handleSuggestionClick = (suggestion) => {
-        setSearch(suggestion);
-        setShowResults(false);
-    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -57,7 +17,6 @@ function Navbar() {
         navigate(`/search?q=${search}`);
         setTimeout(() => {
             setSearch('');
-            setResults([]);
         }, 100);
     }
 
@@ -73,27 +32,17 @@ function Navbar() {
                     <div className='on-submit' onSubmit={handleSubmit} ref={formRef}>
                         <div className="input-container">
                             <input type="text" placeholder="Pesquise um filme"
-                                onChange={handleChange}
+                                onChange={(e) => setSearch(e.target.value)}
                                 value={search}
                                 ref={inputRef}
-                                name="movieSearch"  /* Adicionado atributo name */
+                                name="movieSearch"  
                             />
-                        </div>
-                        <div className="select-container">
-                        <select className={search && showResults ? 'visible' : 'hidden'} size={results.length > 0 ? "5" : "1"}>
-                                {results.map((result) => (
-                                    <option key={result.id} onClick={() => handleSuggestionClick(result.title)}>
-                                        {result.title}
-                                    </option>
-                                ))}
-                            </select>
                         </div>
                     </div>
                     <div className='button'>
                         <button type="button" title='buscar' onClick={handleSubmit}>
                             <BiSearchAlt2 />
                         </button>
-
                     </div>
                 </div>
             </nav >
