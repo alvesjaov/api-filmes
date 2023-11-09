85089faa1a0acc7f073f5e0d4d15bfb0a6407904
alvesjaov <victoralves169@outlook.com>
2023-11-09 16:02:42 -0300

    Refactor: renomeia MoviesGrid para ContentCard

---
+++ src/components/ContentCard.jsx
@@ -0,0 +1,40 @@
+import { FaStar } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+import defaultImage from '../../assets/img/Placeholder.svg'; // Importa a imagem padrão
+
+const imageUrl = import.meta.env.VITE_IMG;
+const notImg = defaultImage;
+
+const ContentCard = ({ content, showLink = true }) => {
+    if (!content) {
+        return null; // or render some fallback UI
+    }
+
+    // Se content.poster_path for nulo, usa a imagem padrão
+    const posterPath = content?.poster_path ? imageUrl + content.poster_path : notImg;
+    const isDefaultImage = posterPath === notImg;
+    const title = content.title || content.name;
+    const detailsLink = window.location.origin + `/${content.contentType}/${content.id}`;
+
+    return (
+        <div className="movie-card">
+            <img src={posterPath} alt={title} className={isDefaultImage ? "no-events" : ""} />
+            <h2>{title}</h2>
+            {showLink && (
+                <>
+                    <p>
+                        <FaStar /> {content?.vote_average?.toFixed(1)}
+                    </p>
+                    <a href={detailsLink}>Ver detalhes</a>
+                </>
+            )}
+        </div>
+    );
+}
+
+ContentCard.propTypes = {
+    content: PropTypes.object.isRequired,
+    showLink: PropTypes.bool
+};
+
+export default ContentCard;
