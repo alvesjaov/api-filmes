f79ccc58f430a7146465359c64a23c4d033cc084
alvesjaov <victoralves169@outlook.com>
2023-11-09 16:35:17 -0300

    Add: estilos ao contêiner de conteúdo e remover isCarregando state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,8 +15,7 @@ const Home = () => {
   const [content, setContent] = useState([]);
   const [genre, setGenre] = useState('top_rated');
   const [page, setPage] = useState(1);
-  const [contentType, setContentType] = useState('movie');
-  const [isLoading, setIsLoading] = useState(false); // Novo estado para controlar o carregamento 
+  const [contentType, setContentType] = useState('movie'); 
 
   const genres = useMemo(() => ({
     'movie': {
@@ -66,8 +65,6 @@ const Home = () => {
     const signal = controller.signal;
 
     const getContent = async (url, contentType) => {
-      setIsLoading(true); // Inicia o carregamento
-
       const res = await fetch(url, { signal });
       if (!signal.aborted) {
         const data = await res.json();
@@ -84,7 +81,6 @@ const Home = () => {
           }, []);
           return newContent;
         });
-        setIsLoading(false); // Termina o carregamento
       }
     }
 
@@ -118,13 +114,7 @@ const Home = () => {
   const loadMoreContent = () => {
     setPage(oldPage => oldPage + 1);
   };
-  let titleText = '';
-  if (isLoading) {
-    titleText = 'Carregando...';
-  } else if (content.length > 0) {
-    titleText = genres[contentType][genre];
-  }
-  
+
   return (
     <div className='container'>
       <div className='content'>
@@ -146,16 +136,15 @@ const Home = () => {
             ))}
           </select>
         </div>
-        <h2 className='title'>{titleText}</h2>
+        <h2 className='title'>{genres[contentType][genre]}</h2>
         <div className='content-container'>
+          {content.length === 0 && <p>Carregando Conteúdo...</p>}
           {content.length > 0 && content.map((item) => <ContentCard key={item.id} content={item} contentType={item.contentType} />)}
         </div>
         {content.length > 0 && <button type='button' title='carregar' onClick={loadMoreContent}>Carregar mais<BiDownArrow /></button>}
       </div>
     </div>
   );
-  
-  };
-  
-  export default Home;
-  
+};
+
+export default Home;
